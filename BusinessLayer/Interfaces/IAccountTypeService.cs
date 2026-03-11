ed86434fd9445081cad4868f572fe1bf0ce5e696
praveennani360 <praveen@cortracker360.com>
2026-03-11 16:47:26 +0530

    AccountType-Praveen
    AccountType-Praveen

---
+++ BusinessLayer/Interfaces/IAccountTypeService.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Common;
+using BusinessLayer.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interfaces
+{
+    public interface IAccountTypeService
+    {
+        Task<ApiResponse<IEnumerable<AccountTypeDto>>> GetAll(int userId);
+
+        Task<ApiResponse<AccountTypeDto?>> GetByIdAsync(int id);
+
+        Task<ApiResponse<string>> CreateAsync(AccountTypeDto dto);
+
+        Task<ApiResponse<string>> UpdateAsync(AccountTypeDto dto);
+
+        Task<ApiResponse<string>> DeleteAsync(int id);
+    }
+}
