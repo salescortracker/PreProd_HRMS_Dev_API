381620a6366348088d7ea6554c95e63e193ac011
Ravichandra2000 <ravichandra@cortracker360.com>
2026-03-11 11:40:10 +0530

    Attendance-List-Task-RaviChandra
    Attendance-List-Task-RaviChandra

---
+++ BusinessLayer/Interfaces/IAttendanceService.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interfaces
+{
+    public interface IAttendanceService
+    {
+        Task<List<EmployeeAttendanceDto>> GetTodayEmployees(int companyId, int regionId);
+
+        Task SaveAttendanceAsync(SaveAttendanceDto dto, int userId);
+
+        Task<List<EmployeeAttendanceDto>> GetWeeklyReport(int companyId, int regionId);
+
+        Task<List<EmployeeAttendanceDto>> GetMonthlyReport(int companyId, int regionId);
+        Task<List<EmployeeAttendanceDto>> GetDateRangeReport(
+           int companyId,
+           int regionId,
+           DateTime fromDate,
+           DateTime toDate);
+    }
+}
---
+++ DataAccessLayer/DBContext/EmployeeAttendance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccessLayer.DBContext;
+
+public partial class EmployeeAttendance
+{
+    public int AttendanceId { get; set; }
+
+    public int? RegionId { get; set; }
+
+    public int? CompanyId { get; set; }
+
+    public string? EmployeeCode { get; set; }
+
+    public string? EmployeeName { get; set; }
+
+    public DateOnly? AttendanceDate { get; set; }
+
+    public string? Status { get; set; }
+
+    public int? CreatedBy { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public string? ModifiedBy { get; set; }
+
+    public DateTime? ModifiedAt { get; set; }
+
+    public TimeOnly? ClockInTime { get; set; }
+
+    public TimeOnly? ClockOutTime { get; set; }
+
+    public string? GrossTime { get; set; }
+}
